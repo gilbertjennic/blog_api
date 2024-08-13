191ca807c80f241ff5325abafd8a6fe576a7331c
Nicole <gilbertjennic@gmail.com>
2024-08-13 18:16:27 -0400

    Basic controller test

---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get posts_url, as: :json
+    assert_response :success
+  end
+end
