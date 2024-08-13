922e65b12ed5db6d0907f93d3a4f02aaec454d6f
Nicole <gilbertjennic@gmail.com>
2024-08-13 18:18:48 -0400

    BonusTask: Pagination: Use the will_paginate or kaminari gem for pagination.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate, only: [:create]
 
   def index
-    posts = Post.all
+    posts = Post.page(params[:page]).per(10)
     render json: posts
   end
 
